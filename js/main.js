a03a972204fe0655074515d9f60d0a097e73d9b9
Dasha <72003879+Bowl-of-noodles@users.noreply.github.com>
2021-12-21 11:19:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,115 @@
+var x, y, r;
+const maxLength = 10;
+const dataType = 'html';
+
+if (dataType === 'json') {
+    window.onload = (event) => {
+        if (document.cookie !== '') {
+            document.cookie.split("|").forEach(function (strElem) {
+                let newRow = generateRowFromElem(JSON.parse(strElem));
+                $('#result-table tr:first').after(newRow);
+            });
+        }
+    };
+}
+
+function setX(element) {
+    x = element.value;
+    [...document.getElementsByName("x")].forEach(function (btn) {
+        btn.style.transform = "";
+    });
+    element.style.transform = "scale(1.3)";
+}
+
+
+function errorMessage(messerror) {
+    alert(messerror);
+}
+
+function isNumber(input) {
+    return !isNaN(parseFloat(input)) && isFinite(input);
+}
+
+function inLength(input) {
+    return input.length <= maxLength;
+}
+
+function validateX() {
+    if (x) return true;
+
+    errorMessage("Выберите x");
+    return false;
+}
+
+
+function validateY() {
+
+    var valY = document.getElementById("y_container").value;
+    valY = valY.replace(",", ".");
+
+    if (valY === undefined) {
+        errorMessage("Поле Y не заполнено");
+        return false;
+    }
+    if (!isNumber(valY) ||!((valY >= -5) && (valY <= 5))) {
+        errorMessage("Y должен быть числом в промежутке [-5;5]");
+        return false;
+    }
+    if (!inLength(valY)) {
+        errorMessage(`Длина числа - не более ${maxLength}`);
+        return false;
+    }
+    y = valY;
+    return true;
+}
+
+function validateR() {
+    const checkboxes = [...document.querySelectorAll("input[name=r]:checked")];
+    if (checkboxes.length === 0) {
+        errorMessage("Выберите хотя бы один R");
+        return false;
+    }
+    r = checkboxes.map(r => r.getAttribute('value')).join(",");
+    return true;
+}
+
+
+
+function submit() {
+    if (validateX() && validateY() && validateR()) {
+        $.get("php/main.php", {
+            'x': x,
+            'y': y,
+            'r': r,
+            'timezone': new Date().getTimezoneOffset(),
+            'wholeTable': false,
+            'dataType': dataType
+        }).done(function (data) {
+            if (dataType === 'json') {
+                let arr = JSON.parse(data);
+                arr.forEach(function (elem) {
+                    if (!elem.validate) return;
+                    let newRow = generateRowFromElem(elem);
+                    $('#result_table tr:first').after(newRow);
+                    if (document.cookie !== '') {
+                        document.cookie += '|' + JSON.stringify(elem)
+                    } else document.cookie = JSON.stringify(elem);
+                });
+            } else if (dataType === 'html') {
+                $('#result_table tr:first').after(data);
+            }
+        });
+    }
+}
+
+function generateRowFromElem(elem) {
+    let newRow = elem.isHit ? '<tr class="hit-yes">' : '<tr class="hit-no">';
+    newRow += '<td>' + elem.x + '</td>';
+    newRow += '<td>' + elem.y + '</td>';
+    newRow += '<td>' + elem.r + '</td>';
+    newRow += '<td>' + elem.currentTime + '</td>';
+    newRow += '<td>' + elem.execTime + '</td>';
+    newRow += '<td>' + (elem.isHit ? 'Да' : 'Нет') + '</td>';
+
+    return newRow;
+}
---
+++ php/main.php
@@ -0,0 +1,99 @@
+<?php
+require_once("jsonEncode.php");
+
+function checkRectangle($x, $y, $r) {
+    return $x <= 0 && $y >= 0 && $x <= -$r/2 && $y <= $r;
+}
+
+function checkTriangle($x, $y, $r) {
+    return $x >= 0 && $y >= 0 && $y + $x <= $r ;
+}
+
+function checkCircle($x, $y, $r) {
+    return $x <= 0 && $y <= 0 && sqrt($x * $x + $y * $y) <= $r / 2;
+}
+
+function checkHit($x, $y, $r) {
+    return checkCircle($x, $y, $r) || checkRectangle($x, $y, $r) || checkTriangle($x, $y, $r);
+}
+
+function validateForm($x, $y, $r)
+{
+    return -5 <= $x and $x <= 3 and -5 <= $y and $y <= 5 and 1 <= $r and $r <= 5;
+}
+
+function validateTimezone($timezoneOffset) {
+    return isset($timezoneOffset) && is_numeric($timezoneOffset) && $timezoneOffset % 60 === 0;
+}
+
+function getResultArray($x, $y, $r, $timezone) {
+    $results = array();
+
+    foreach ($r as $value) {
+        $isValid = validateForm($x, $y, $value);
+        $isHit = checkHit($x, $y, $value);
+        $currentTime = date('H:i', time() - $timezone * 60);
+        $executionTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 7);
+
+        array_push($results, array(
+            "validate" => $isValid,
+            "x" => $x,
+            "y" => $y,
+            "r" => $value,
+            "currentTime" => $currentTime,
+            "execTime" => $executionTime,
+            "isHit" => $isHit
+        ));
+    }
+
+    return $results;
+}
+
+function generateTableWithRows($wholeTable, $results) {
+    $html = $wholeTable == 'true'? '<table id="result_table"><tr class="first">
+        <th class="coords-col">X</th>
+        <th class="coords-col">Y</th>
+        <th class="coords-col">R</th>
+        <th class="time-col">Время запроса</th>
+        <th class="time-col">Время исполнения</th>
+        <th class="hitres-col">Попадание</th>
+    </tr>': '';
+
+    foreach ($results as $elem)
+        $html .= generateRow($elem);
+
+    if ($wholeTable == 'true') $html .= '</table>';
+    return $html;
+}
+
+function generateRow($elem) {
+    $isHit = $elem['isHit'] ? 'Да': 'Нет';
+    $elemHtml = $elem["isHit"]? '<tr class="hit-yes">' : '<tr class="hit-no">';
+    $elemHtml .= '<td>' . $elem['x'] . '</td>';
+    $elemHtml .= '<td>' . $elem['y'] . '</td>';
+    $elemHtml .= '<td>' . $elem['r'] . '</td>';
+    $elemHtml .= '<td>' . $elem['currentTime'] . '</td>';
+    $elemHtml .= '<td>' . $elem['execTime'] . '</td>';
+    $elemHtml .= '<td>' . $isHit . '</td>';
+    $elemHtml .= '</tr>';
+
+    return $elemHtml;
+}
+
+
+$x = $_GET['x'];
+$y = $_GET['y'];
+$r =  explode(",", $_GET['r']);
+$dataType = $_GET['dataType'];
+$wholeTable = $_GET['wholeTable'];
+
+if (!isset($wholeTable)) $wholeTable = true;
+
+$timezone = $_GET['timezone'];
+
+$results = getResultArray($x, $y, $r, $timezone);
+
+if ($dataType == 'json')
+    echo toJSON($results);
+else if($dataType == 'html')
+    echo generateTableWithRows($wholeTable, $results);
